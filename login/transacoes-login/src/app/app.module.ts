2f99b8989aba9276055c6e6488a3812f1bdb7c78
jjohnnys <johnnysilva85@gmail.com>
2022-09-10 16:45:36 -0300

    andanmento desenvolvimento tela de login

--- login/transacoes-login/src/app/app.module.ts
+++ login/transacoes-login/src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { HomeModule } from './home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,8 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
